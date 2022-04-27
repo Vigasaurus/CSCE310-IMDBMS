100899a2c1de00e28ce24562d2fcde0b54e2bf4d
davidtang332 <davidtang332@gmail.com>
2022-04-27 16:27:12 -0500

    Updated featured_list and watchlist feature sets

--- routes/featured_list.js
+++ routes/featured_list.js
@@ -4,22 +4,41 @@ const sql = require('../postgres');
 
 // Gets current featured list - should check the date range using current datetime
 router.get('/featured', async function (req, res) {
+	//todo
+	const today = new Date().getTime();
+	const query = await sql`
+		SELECT * FROM featured_movies
+		WHERE ${today} 
+	`
+
 	res.send({});
 });
 
 // Adds new item to featured list - use req.body for new data (Vig will handle admin-only authorization)
 router.post('/featured', async function (req, res) {
-	res.send({});
+	const query = await sql`
+		INSERT INTO featured_movies(week, movie_id, creator_id, index)
+		VALUES (${req.body}, ${req.body}, ${req.body}, 1)
+	`
+	res.send({query});
 });
 
 // Updates a featured list item - use req.params.id for item id, use req.body for updated index
 router.patch('/featured/:id', async function (req, res) {
-	res.send({});
+	const query = await sql`
+		UPDATE featured_movies
+		SET week = ${req.body}, movie_id = ${req.body}, creator_id = ${req.body}, index = ${req.body}
+		WHERE id = ${req.params.id}
+	`
+	res.send({query});
 });
 
 // Delte a featured list item - use req.params.id for item id
 router.delete('/featured/:id', async function (req, res) {
-	res.send({});
+	const query = await sql`
+		DELETE FROM featured_movies WHERE id = ${req.params.id}
+	`
+	res.send({query});
 });
 
 module.exports = router;
--- routes/watchlist.js
+++ routes/watchlist.js
@@ -4,22 +4,43 @@ const sql = require('../postgres');
 
 // Gets watchlist for a user - use req.params.user for user id
 router.get('/watchlist/:user', async function (req, res) {
-	res.send({});
+	console.log(req.params.id);
+	console.log(req.body);
+	const query = await sql`
+		SELECT * FROM watches where user_id = ${req.params.user}
+	`
+	res.send({query});
 });
 
 // Adds new watchlist item for a user - use req.params.user for user id
 router.post('/watchlist/:user', async function (req, res) {
-	res.send({});
+	console.log(req.params.id);
+	const user_id = req.params.id;
+	
+	const query = await sql`
+		INSERT INTO watches(user_id, movie_id, index)
+		VALUES (${req.params.user}, ${req.body}, 1})
+
+	`
+	res.send({query});
 });
 
 // Updates a watchlist item for a user - use req.params.id for watchlist item id, use req.body for updated data
 router.patch('/watchlist/:id', async function (req, res) {
-	res.send({});
+	const query = await sql`
+		UPDATE watches 
+		SET movie_id = ${req.body}, index = ${req.body}
+		WHERE id = ${req.params.id} AND user_id = ${req.params.user}
+	`
+	res.send({query});
 });
 
 // Deletes a watchlist item for a user - use req.params.id for watchlist item id
 router.delete('/watchlist/:id', async function (req, res) {
-	res.send({});
+	const query = await sql`
+		DELETE FROM watches where id = ${req.params.id} AND user_id = ${req.params.user}
+	`
+	res.send({query});
 });
 
 module.exports = router;
